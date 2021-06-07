b25bf324add386992051ade0e1fb3ada9e72a08c
Shubroto Shuvo <shubroto.debnath@gmail.com>
2021-06-07 00:33:43 -0500

    Initial Commit

---
+++ AnacoreKit/AnacoreKit.h
@@ -0,0 +1,18 @@
+//
+//  AnacoreKit.h
+//  AnacoreKit
+//
+//  Created by Shubroto Shuvo on 6/7/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AnacoreKit.
+FOUNDATION_EXPORT double AnacoreKitVersionNumber;
+
+//! Project version string for AnacoreKit.
+FOUNDATION_EXPORT const unsigned char AnacoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AnacoreKit/PublicHeader.h>
+
+
